d7909004ca606c3f946e60bb0bf0683059e9ddd8
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-09-25 20:13:25 -0400

    Appointment controller

---
+++ fase2/modulo02/gobarber/src/app/controllers/AvailableController.js
@@ -0,0 +1,83 @@
+import * as Yup from 'yup';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+
+/**
+ * List all available spots from a given provider.
+ * @author Leonardo Otoni
+ */
+class AvailableController {
+  async index(req, res) {
+    const schema = Yup.object().shape({
+      providerId: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    const searchDate = Number(req.query.date);
+    try {
+      await schema.validate({
+        providerId: Number(req.params.providerId),
+        date: new Date(searchDate),
+      });
+    } catch (error) {
+      return res.status(400).json({ message: error.errors });
+    }
+
+    // Get all provider's appointments not canceled from a specific date
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    // Default Appointment Time Available
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(available);
+  }
+}
+
+export default new AvailableController();
