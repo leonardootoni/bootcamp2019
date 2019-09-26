bfe2d7cdef622d1ae036a28cda080ce59dd9f2d8
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-09-25 20:16:21 -0400

    included Sentry and Youch config

--- fase2/modulo02/gobarber/src/app.js
+++ fase2/modulo02/gobarber/src/app.js
@@ -1,8 +1,13 @@
+import 'dotenv/config';
 import express from 'express';
-import path from 'path';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors'; // It has to be imported before the app routes.
+import Youch from 'youch';
 
+import path from 'path';
 import cors from 'cors';
 import helmet from 'helmet';
+import sentryConfig from './config/sentry';
 
 import routes from './routes';
 import './database';
@@ -10,11 +15,18 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig); // It must be loaded after everything else
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // The request handler must be the first middleware on the app
+    this.server.use(Sentry.Handlers.requestHandler());
+
     this.server.use(helmet());
     this.server.use(cors());
     this.server.use(express.json());
@@ -26,6 +38,32 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  /**
+   * Load a general exception handler middleware to intercept all application
+   * errors. It makes use of express-async-errors lib.
+   */
+  exceptionHandler() {
+    /**
+     * Sentry Error Handler middleware. It must be loaded after all routes.
+     */
+    this.server.use(Sentry.Handlers.errorHandler());
+
+    /**
+     * General Application Exception Handler. It will intercept all application
+     * exceptions and respond a JSON statck trace wrapped into a http 500 error
+     * response body for all not handled exceptions.
+     */
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        // Convert a error stacktrace into JSON format.
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status.json({ error: 'Internal server error.' });
+    });
   }
 }
 
