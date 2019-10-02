de6eaea5c5a89aa9d831eb100bfebdc5c86b9979
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-01 20:27:30 -0400

    change to make use of Env variable

--- fase2/desafio2/src/config/database.js
+++ fase2/desafio2/src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv/config');
+
 // Sequelize database config file
 module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'desafio2',
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   define: {
     timestamps: true,
     underscored: true,
