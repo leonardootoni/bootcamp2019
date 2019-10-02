cfdf4cbd18e43af250efd92000fb213d0a078b2a
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-01 20:52:18 -0400

    Included Javadoc

--- fase2/modulo02/gobarber/src/app/controllers/FileController.js
+++ fase2/modulo02/gobarber/src/app/controllers/FileController.js
@@ -1,6 +1,14 @@
 import File from '../models/File';
 
 class FileController {
+  /**
+   * Save updloaded file data into the database.
+   * It gets from multer the following metadata:
+   * originalname: Nome of the file on the user's computer
+   * filename: The name of the file within the destination (diskstorage)
+   * @param {Object} req - Request
+   * @param {Object} res - Response
+   */
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
     const file = await File.create({
