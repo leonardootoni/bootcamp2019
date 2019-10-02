2fc5ec87690a38a8e1949ecd27c9292be5f52bdb
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-01 20:22:10 -0400

    Included Youch and general exception handler.

--- fase2/desafio2/src/app.js
+++ fase2/desafio2/src/app.js
@@ -1,6 +1,10 @@
+import 'dotenv/config';
+import { resolve } from 'path';
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import 'express-async-errors'; // It has to be imported before app routes
+import Youch from 'youch';
 
 import './database';
 import routes from './routes';
@@ -14,19 +18,41 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   // Define all application middlewares
   middlewares() {
     // enable secure response http headers
     this.server.use(helmet());
-    this.server.use(express.json());
     this.server.use(cors());
+
+    this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   // define the application main routes file
   routes() {
     this.server.use(routes);
+  }
+
+  /**
+   * General Application Exception Handler.
+   */
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        // Convert a error stacktrace into JSON format.
+        console.log(err);
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error.' });
+    });
   }
 }
 
