ce997da6277e4b2acec349cde1323ab39a4b930f
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-01 20:22:32 -0400

    New routes challenge 3

--- fase2/desafio2/src/routes.js
+++ fase2/desafio2/src/routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authValidator from './app/middlewares/auth';
+import pagination from './app/middlewares/pagination';
 
+import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
 import SessionController from './app/controllers/SessionController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 import UserController from './app/controllers/UserController';
 
-// Application routes file
+/** ***************************************************************************
+ *  Instance configuration
+ * ************************************************************************** */
 const routes = new Router();
+const upload = multer(multerConfig);
 
 /** ***************************************************************************
  * Unprotected routes goes here
@@ -15,13 +24,23 @@ routes.post('/auth', SessionController.store);
 routes.post('/users', UserController.store);
 
 /** ***************************************************************************
- * Security Route Middleware
+ * Security Route Middlewares
  * ************************************************************************** */
 routes.use(authValidator.checkToken);
+routes.use(pagination);
 
 /** ***************************************************************************
  * Protected routes goes here
  * ************************************************************************** */
+routes.get('/files', FileController.index);
+routes.post('/files', upload.single('file'), FileController.store);
+routes.delete('/meetup/:id', MeetupController.delete);
+routes.get('/meetup', MeetupController.index);
+routes.post('/meetup', MeetupController.store);
+routes.put('/meetup/:id', MeetupController.update);
+routes.get('/subscription', SubscriptionController.index);
+routes.post('/subscription', SubscriptionController.store);
+routes.delete('/subscription/:meetupId', SubscriptionController.delete);
 routes.put('/users/:id/updatePassword', UserController.update);
 
 export default routes;
