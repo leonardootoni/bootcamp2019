5c2f7921b5690c590269373207e2ee313df0f9dd
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-01 20:51:29 -0400

    Included Subscription and Meetup Models

--- fase2/desafio2/src/database/index.js
+++ fase2/desafio2/src/database/index.js
@@ -1,9 +1,13 @@
 import Sequelize from 'sequelize';
 
 import databaseConfig from '../config/database';
+
+import File from '../app/models/File';
+import Meetup from '../app/models/Meetup';
+import Subscription from '../app/models/Subscription';
 import User from '../app/models/User';
 
-const models = [User];
+const models = [File, Meetup, Subscription, User];
 
 /**
  * Load the database configuration and initialize all declared models
@@ -15,7 +19,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
