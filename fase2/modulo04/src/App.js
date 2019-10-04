57541d719959ec8d4b15b34ac9b22844cf4c89f6
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-03 23:23:23 -0400

    Base React Skelleton

---
+++ fase2/modulo04/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import background from './assets/road-1072823_960_720.jpg';
+
+function App() {
+  return <img src={background} />;
+}
+
+export default App;
