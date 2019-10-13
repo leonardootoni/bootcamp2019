9f3f3b1c4862e456ef573b21d0332f472a101cc3
Leonardo Otoni de Assis <leonardootonideassis@Leonardos-MacBook-Pro.local>
2019-10-13 14:01:20 -0400

    Post Component

--- fase2/desafio4-react/src/components/Post/index.js
+++ fase2/desafio4-react/src/components/Post/index.js
@@ -1,13 +1,48 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import './styles.css';
+import PostComment from './PostComment';
 
 class Post extends Component {
   render() {
+    const post = this.props.post;
     return (
-      <Fragment>
-        <h1>Post Component</h1>
-      </Fragment>
+      <div className='post-card'>
+        <div className='post-header'>
+          <div className='post-header-item'>
+            <img src={post.author.avatar}></img>
+          </div>
+          <div className='post-header-item'>
+            <div className='post-header-submitter'>{post.author.name}</div>
+            <div className='post-header-submitter-date'>{post.dateInfo}</div>
+          </div>
+        </div>
+        <div>
+          {post.content}
+          <hr />
+        </div>
+        <div>
+          {post.comments.map(comment => {
+            return <PostComment key={comment.id} comment={comment} />;
+          })}
+        </div>
+      </div>
     );
   }
 }
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired
+    }),
+    dateInfo: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired
+  }).isRequired
+};
 
 export default Post;
